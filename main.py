920c4cb244ce4fb525e7cbbdea697015d691415e
Botong Sui <suiyxfx@gmail.com>
2017-02-07 20:17:59 -0500

    sloppy homework

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ import os
 import jinja2
 import webapp2
 import logging
+import json
+import urllib
+
+# this is used for constructing URLs to google's APIS
+from googleapiclient.discovery import build
+
+API_KEY = 'AIzaSyADiwBc0are9WMVyRRiPDz1ZIuZPA0OkyY'
+TABLE_ID = '1O3JunhDFtViea0ubyBRqkpgbcbn0O13WHffgEsSO'
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+    
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+#service = build('fusiontables', 'v1', developerKey=API_KEY)
+#request = service.column().list(tableId=TABLE_ID)
+#query = "SELECT * FROM " + TABLE_ID + " WHERE  Status = 'Accepted' LIMIT 50"
+#response = service.query().sql(sql=query).execute()
+#cols = response['columns']
+#logging.info(cols)
+#rows = response['rows']
+#logging.info(response['rows'])
     
 from flask import Flask
 app = Flask(__name__)
@@ -27,11 +46,19 @@ def table():
     template = JINJA_ENVIRONMENT.get_template('templates/table.html')
     return template.render()
 
-@app.route('/teamInfo')
-def teamInfo():
-    template = JINJA_ENVIRONMENT.get_template('templates/teamInfo.html')
-    return template.render()
+#def get_all_data(self):
+#    query = "SELECT * FROM " + TABLE_ID + " WHERE  AnimalType = 'DOG' LIMIT 2"
+#    response = service.query().sql(sql=query).execute()
+#    cols = response['columns']
+#    logging.info(cols)
+#    rows = response['rows']
+#    logging.info(response['rows'])
+#    return template.render(headers=cols, content=rows)
 
+@app.route('/quality')
+def quality():
+	template = JINJA_ENVIRONMENT.get_template('templates/quality.html')
+	return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
